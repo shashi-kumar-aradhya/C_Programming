ec20672b4072a0c4d2e2c3ab8eecc8583c80f9d1
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2016-10-27 09:24:57 +0530

    Adding library implementation
    Signed-off-by: shashi-kumar-aradhya <shashikumararadhya@gmail.com>

---
+++ Library_Imp/static/addDemo.c
@@ -0,0 +1,11 @@
+//#include <heymath.h>
+#include "heymath.h"
+#include <stdio.h>
+ 
+int main()
+{
+  int x = 10, y = 20;
+//  printf("\n%d + %d = %d\n", x, y, add(x, y));
+//  printf("\n%d + %d = %d\n", x, y, sub(x, y));
+  return 0;
+}

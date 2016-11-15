f18f1ef9d2bdb96b701fd2f108d21360f58a1fa2
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2016-11-16 00:19:31 +0530

    Added singly linked list insert function
    Signed-off-by: shashi-kumar-aradhya <shashikumararadhya@gmail.com>

--- DS/LinkedList/Linked.c
+++ DS/LinkedList/Linked.c
@@ -1,3 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Linked.h"
+
+node * create_node(int value) 
+{
+	node * temp = NULL;
+	temp = (node *)malloc(sizeof(node));
+	if (NULL == temp) {
+		return NULL;
+	}
+	temp->value = value;
+	temp->next = NULL;
+	return temp;
+}
+
+ret insert_front(node ** head, int value)
+{
+	node *temp = create_node(value);
+	if (NULL == temp) {
+		return FAILURE;
+	}
+	temp->next = *head;
+	*head = temp;
+	return SUCCESS;
+}
+ret insert_rear(node **head, int value)
+{
+	node *temp = create_node(value);
+	node *ptr = *head;
+	if (NULL == temp) {
+		return FAILURE;
+	}
+	if (NULL == *head) {
+		*head = temp;
+		return SUCCESS;
+	}
+	while (NULL != ptr->next) {
+		ptr = ptr->next;
+	}
+	ptr->next = temp;
+	return SUCCESS;
+}
+ret insert_after(node *head, int value)
+{
+	node *temp = create_node(value);
+	if (NULL == temp) {
+		return FAILURE;
+	}
+	temp->next = head->next;
+	head->next = temp;
+	return SUCCESS;
+}
+
+void display(node *head)
+{
+	while (NULL != head) {
+		printf("Value = %d\n", head->value);
+		head = head->next;
+	}
+	return;
+}
 
--- interviews/UndoRedo/test.c
+++ interviews/UndoRedo/test.c
@@ -9,13 +9,13 @@ int main()
 	char buffer[100];
 	int i;
 	ts_init(&ts, 500);
-	for(i = 0; i < 1; i++) {
+	for(i = 0; i < 4; i++) {
 		printf("Enter the string : ");
 		fgets(buffer, sizeof(buffer), stdin);
 		*(buffer + strlen(buffer)-1) = '\0';
 		ts_push_state(&ts, buffer);
 	}
-	for(i = 0; i < 3; i++) {
+	for(i = 0; i < 4; i++) {
 		ts_undo(&ts);
 	}
 	return 0;

ca587f510295d67af0b3a91e109173448dfb8247
shashi <shashikumararadhya@gmail.com>
2018-10-25 22:42:37 +0530

    double_pointer

--- Pointers/double_pointer.c
+++ Pointers/double_pointer.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-#define OVERRIDE_INDEX_NUM	5 
+#define INDEX_NUM	5 
 
 typedef enum {
-	MTPM,
-	BDSAR,
-	RTSAR,
-	SHM,
-	UTA,
-	MTPC_MAX,
-}MTPC_t;
+	ZERO,
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	MAX,
+}MAXINPUT_t;
 
-unsigned int mtpc[MTPC_MAX+1][OVERRIDE_INDEX_NUM];
+unsigned int mtpc[MAX+1][INDEX_NUM];
 
-void LookUpInvalid(unsigned int input[][OVERRIDE_INDEX_NUM])
+void LookUpInvalid(unsigned int input[][INDEX_NUM])
 {
 	int i;
-	for (i = 0; i < OVERRIDE_INDEX_NUM; i++)
+	for (i = 0; i < INDEX_NUM; i++)
 	{
 		(*input)[i] = 0xff;
 		printf("LookUp : %p, i = %d\n", &((*input)[i]), i);
@@ -32,8 +32,8 @@ void binary_search(unsigned int *array)
 int main()
 {
 	int i = 0;
-	printf("Total size = %ld, MTPC_MAX = %d\n", sizeof(mtpc), MTPC_MAX);
-	for(i = 0; i <=MTPC_MAX; i++)
+	printf("Total size = %ld, MTPC_MAX = %d\n", sizeof(mtpc), MAX);
+	for(i = 0; i <=MAX; i++)
 	{
 		printf("client = %d\t address = %p\n", i, &mtpc[i]);
 		LookUpInvalid(&mtpc[i]);

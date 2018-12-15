5655332baf23ed09c76af18ecc8c5c38b84fefba
shashi <shashikumararadhya@gmail.com>
2018-12-15 20:30:01 +0530

    Typedef of Array to pointer

---
+++ Pointers/new_2/format_specifiers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int a = 66;
+	printf("unsigned char = %p", (unsigned char)a);
+	printf("\n");
+	printf("signed char = %p", (signed char)a);
+	printf("\n");
+	printf("unsigned int = %p", (unsigned int)a);
+	printf("\n");
+	printf("unsigned short int = %p", (unsigned short int)a);
+	printf("\n");
+	printf("int = %p", (int)a);
+	printf("\n");
+	printf("short int = %p", (short int)a);
+	printf("\n");
+	return 0;
+}

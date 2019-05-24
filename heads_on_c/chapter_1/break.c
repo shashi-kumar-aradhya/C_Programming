a3e92bf2b5ae4a3bd2218115b887380d60199fd6
shashi <shashikumararadhya@gmail.com>
2019-05-24 23:29:00 +0530

    thread link addition

---
+++ heads_on_c/chapter_1/break.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	{
+	if (1)
+	{
+		printf("I am at first line\n");
+		break;
+		printf("I am at second line\n");
+	}	
+		printf("I am at third line\n");
+	}
+	return 0;
+}
